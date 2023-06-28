f922a950e2f03aa3ecaa8acaca94faf77c034e40
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-06-28 16:22:30 -0300

    Create import.sql for seed BD.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renato Augusto', '015.256.158-85', 1200.00, '1995-12-15', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Assis Silva', '025.635.895-00', 1350.00, '1987-06-14', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Guilherme Rocha', '095.253.188-95', 1800.00, '1971-04-03', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Guilherme', '045.285.156-14', 1587.00, '2015-03-14', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Denis Fonseca', '074.145.415-15', 1151.00, '2010-04-21', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Andre', '028.985.741-46', 1256.00, '1981-08-26', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Flavio Silva', '026.756.956-02', 1600.00, '1979-07-17', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Fernandes', '014.852.251-56', 1560.00, '1982-03-23',5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Andrade', '085.475.758-18', 3000.00, '1985-06-25', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fabiola Sá', '014.252.178-95', 3500.00, '1997-11-13', 2);
+
